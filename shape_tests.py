a6d9db0aad610a24341940e3374a19e6fecd72f2
kishanveka <kishanveka@live.com>
2020-11-20 17:51:45 -0800

    Adding shape_tests.py which will have unit tests for shape.py.

---
+++ shape_tests.py
@@ -0,0 +1,17 @@
+import unittest
+from shape import Shape
+
+class CircleTests(unittest.TestCase):
+    """
+    This class tests functionality provided by the Shape class.
+    """
+    def test_init(self):
+        shape = Shape()
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
