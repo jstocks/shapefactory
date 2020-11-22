97332043794845f48d2a627c1e7baa143cdfbf05
jstocks <72317128+jstocks@users.noreply.github.com>
2020-11-21 21:37:32 -0800

    # removed instances of sample shape creation

--- shapefactory.py
+++ shapefactory.py
@@ -27,31 +27,3 @@ class ShapeFactory:
         """creates a triangle based on the class Shape and child class triangle"""
         return Triangle(float(base_length), float(height), float(side_length1),
                         float(side_length2))
-
-
-a_circle = Circle(1)
-print(a_circle.name())
-print(a_circle.area())
-print(a_circle.perimeter())
-print(a_circle)
-print(a_circle.draw())
-
-a_rect = Rectangle(2, 3)
-print(a_rect.name())
-print(a_rect.area())
-print(a_rect.perimeter())
-print(a_rect)
-print(a_rect.draw())
-
-a_sq = Square(2)
-print(a_sq.name())
-print(a_sq.area())
-print(a_sq.perimeter())
-print(a_sq)
-print(a_sq.draw())
-a_tri = Triangle(3, 4.5, 4, 5)
-print(a_tri.name())
-print(a_tri.area())
-print(a_tri.perimeter())
-print(a_tri)
-print(a_tri.draw())
